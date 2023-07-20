0da60eacc51f5f0751485aafa01e59eeae945087
Damir <subkhh@gmail.com>
2023-07-20 16:07:14 +0500

    замыкания, декораторы

---
+++ 200723_Zamikaniya.py
@@ -0,0 +1,8 @@
+# замыкания
+
+def main_fun():
+
+    def inner_fun():
+        print('hello my friend')
+
+    return inner_fun()
---
+++ 200723_lesson10_getter_setter.py
@@ -0,0 +1,23 @@
+
+
+# Property. getter method and setter method
+
+class BankAccount:
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
+
+    def get_balance(self):
+        return  self.__balance
+
+    def set_balance(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError('Баланс должен быть числом')
+        self.__balance = value
+
+    def delete_balance(self):
+        print('delete balance')
+        del self.__balance
+
+    balance = property(fget=get_balance,fset=set_balance, fdel=delete_balance)
